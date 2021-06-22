b99239473edcaf242221d2eb60570baade9b49db
straxFromIbr <haga.yuya2.2016@gmail.com>
2021-06-22 18:49:54 +0900

    add レポート

--- tweetquizzes/expiv/settings.py
+++ tweetquizzes/expiv/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-from .local_setting.secretkey import SECRET_KEY
+from .local_settings.secretkey import SECRET_KEY
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
